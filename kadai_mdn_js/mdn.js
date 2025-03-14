fc41a10e833a7f7f3f94853c95d0e090a58b1601
aoyagi959baseball <kabaseball0509@gmail.com>
2025-03-15 04:21:18 +0900

    提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,7 @@
-const today = new data();
-console.log(today)
+const today = new Date();
+const [month, day, year ] =[
+  today.getMonth() + 1,
+  today.getDate(),
+  today.getFullYear(),
+];
+console.log(year + '年',month + '月',day + '日')
